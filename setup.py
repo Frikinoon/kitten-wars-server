dd9192cf8306b7c3718c327c0356350eeed795eb
JesusMtnez <jesusmartinez93@gmail.com>
2016-08-06 00:16:06 +0200

    Add project 'setup.py'

---
+++ setup.py
@@ -0,0 +1,14 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'name': 'Kitten Wars Server',
+    'description': 'Kitten Wars Server',
+    'author': 'JesusMtnez',
+    'author_mail': 'jesusmartinez93@gmail.com',
+    'url': 'https://github.com/frikinoon/kitten-wars-server',
+    'version': '0.0.1-rc01',
+    'install_requires': []
+}
