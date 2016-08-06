0a0e8480f23e7109e7b4790ac92ae6d2ba50c4b7
JesusMtnez <jesusmartinez93@gmail.com>
2016-08-06 12:05:35 +0200

    Fix Connection Closed exception logging

--- server.py
+++ server.py
@@ -5,14 +5,13 @@ import logging
 logging.basicConfig(level=logging.INFO)
 
 async def hello(websocket, path):
-    print("Thread {}".format(asyncio.get_event_loop().__str__()))
     while True:
         try:
             echo = await websocket.recv()
             logging.info("Message received: {}".format(echo))
             await websocket.send(echo)
         except websockets.exceptions.ConnectionClosed as ex:
-            print("Connection Closed {}".format(ex.code))
+            logging.info("Connection Closed with code {}".format(ex.code))
             break
 
 start_server = websockets.serve(hello, '0.0.0.0', 8080)
