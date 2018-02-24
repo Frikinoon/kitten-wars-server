eea11f22427489290dfc96ba712440870cf5ffa3
JesusMtnez <jesusmartinez93@gmail.com>
2018-02-24 18:24:02 +0100

    Fix board tests

--- tests/test_domain/test_board.py
+++ tests/test_domain/test_board.py
@@ -1,7 +1,6 @@
 from nose.tools import eq_, raises
 from domain.kitten import Kitten
 from domain.board import *
-from domain.board_exceptions import *
 
 
 def test_board_instantiation():
@@ -9,8 +8,9 @@ def test_board_instantiation():
     # Fixtures
     height = 10
     width = 20
+    board_spec = BoardSpec(height, width)
     # Actual test
-    board = Board(height, width)
+    board = Board(board_spec)
     # Assertions
     eq_(board.height, height)
     eq_(board.width, width)
@@ -20,7 +20,7 @@ def test_board_instantiation():
 def test_add_non_existing_kitten():
     """Check adding a new kitten to the board works properly"""
     # Fixture
-    board = Board(20, 20)
+    board = Board(BoardSpec(height=20, width=20))
     new_kitten = Kitten("Kitten Test", 10, 10)
     # Actual test
     board.add_kitten(new_kitten)
@@ -32,7 +32,7 @@ def test_add_non_existing_kitten():
 def test_add_existing_kitten_raise_exception():
     """Check adding existing kitten to the board raise KittenAlreadyPlayingException"""
     # Fixtures
-    board = Board(20, 20)
+    board = Board(BoardSpec(height=20, width=20))
     new_kitten = Kitten("Kitten test", 10, 10)
     board.add_kitten(new_kitten)
     # Actual test
@@ -43,7 +43,7 @@ def test_add_existing_kitten_raise_exception():
 def test_adding_kitten_in_impossible_width():
     """Check adding kitten in impossible location width throws KittenInImpossibleLocationException"""
     # Fixture
-    board = Board(10, 10)
+    board = Board(BoardSpec(height=10, width=10))
     new_kitten = Kitten("kitten", 20, 10)
     # Actual test
     board.add_kitten(new_kitten)
@@ -53,7 +53,7 @@ def test_adding_kitten_in_impossible_width():
 def test_adding_kitten_in_impossible_height():
     """Check adding kitten in impossible location height throws KittenInImpossibleLocationException"""
     # Fixture
-    board = Board(10, 10)
+    board = Board(BoardSpec(height=10, width=10))
     new_kitten = Kitten("kitten", 10, 20)
     # Actual test
     board.add_kitten(new_kitten)
